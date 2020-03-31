38ae8399b8549f92884eeaeaa906f6bf57d4b668
Ludde86 <bslbjorn@gmail.com>
2020-03-31 14:59:19 +0200

    add progress to skills component

--- src/components/skills.js
+++ src/components/skills.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Grid, Cell } from 'react-mdl';
+import { Grid, Cell, ProgressBar } from 'react-mdl';
 
 const Skills = (props) => {
 	return (
 		<Grid>
 			<Cell col={12}>
-				<div style={{ display: 'flex' }}>{props.skill}</div>
+				<div style={{ display: 'flex' }}>
+					{props.skill} <ProgressBar style={{ margin: 'auto', width: '75%' }} progress={props.progress} />
+				</div>
 			</Cell>
 		</Grid>
 	);
