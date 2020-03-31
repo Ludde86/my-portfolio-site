8b5cbf118666adddbaabf54d671f3bd49701401f
Ludde86 <bslbjorn@gmail.com>
2020-03-31 09:42:02 +0200

    add paths for all pages

--- src/components/main.js
+++ src/components/main.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { Route, Switch } from 'react-router';
 import Landing from './landingpage';
+import Resume from './resume';
+import Projects from './projects';
+import About from './aboutme';
+import Contact from './contact';
 
 const Main = () => (
 	<Switch>
 		<Route exact path="/" component={Landing} />
+		<Route path="/resume" component={Resume} />
+		<Route path="/projects" component={Projects} />
+		<Route path="/about" component={About} />
+		<Route path="/contact" component={Contact} />
 	</Switch>
 );
 
