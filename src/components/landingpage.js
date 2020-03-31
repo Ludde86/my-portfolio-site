4c560d7d9e20a75bf72aaa6d6eb23535ce4b67d9
Ludde86 <bslbjorn@gmail.com>
2020-03-31 10:48:02 +0200

    add avatar omg and banner text

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { Grid, Cell } from 'react-mdl';
+import myAvatar2 from '../img/myAvatar2.png';
 
 const Landing = () => {
 	return (
 		<div style={{ width: '100%', margin: 'auto' }}>
 			<Grid className="landing-grid">
-				<Cell col={12} />
+				<Cell col={12}>
+					<img src={myAvatar2} alt="profile" className="avatar-img" />
+					<div className="banner-text">
+						<h1>Full Stack Web Developer</h1>
+					</div>
+				</Cell>
 			</Grid>
 		</div>
 	);
