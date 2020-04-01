59e38c3db7686da1538f5a39ad9ed658f274d63a
Ludde86 <bslbjorn@gmail.com>
2020-04-01 10:31:28 +0200

    fix aboutmepage

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -10,13 +10,14 @@ const About = () => {
 				för att så småningom hitta ett jobb som utvecklare innom programmering. Har en praktikperiod kvar på tre
 				månader att slutföra, då via utbildningen, och den räknar jag tidigast med att påbörja nu till hösten.
 				Under tiden fram tills dess, och även under min kommande praktik, så önskar jag om behov finns att
-				arbeta hos er varannan helg, eventuellt även vid behov på vardagar.
+				arbeta hos er varannan helg, eventuellt även vid behov på vardagar. Ränkar med andra ord att isf. få
+				arbeta hos er, minst året ut.
 			</p>
 			<p>
 				Jag har arbetat inom livsbedelsbranchen i nu 12år (10år ute på golvet, 2år i kassan), så är väl medveten
 				om hur viktigt det är att arbeta för sina kollegor, att man arbetar åt samma håll. Och för att en butik
 				ska lyckas gäller det att ha hög kundservice, så att de vill komma tillbaks, eller ännu bättre, sprida
-				kundbemötandet vidare till andra. Ränkar med andra ord att isf. få arbeta hos er, minst året ut.
+				kundbemötandet vidare till andra.
 			</p>
 			<p>
 				Har nu jobbat i över 10år på Ica Maxi Haninge, på mejeriavdelningen, och de två senaste åren har jag
@@ -32,12 +33,11 @@ const About = () => {
 			<p>
 				Hos er vill jag arbeta, dels för det jag tidigare nämnt, men även för att jag är intresserad teknik och
 				prylar, och tror att jag skulle passa in utmärkt hos er. Är väldigt lättillgänglig, och bor bilvägen ca
-				7minuter ifrån er. Har en hög arbetsmoral, och tycker om att arbeta när det är lite ös.
+				7minuter ifrån er. Har en hög arbetsmoral, och tycker om att arbeta när det är lite ös. Truckkort har
+				jag sedan flera år tillbaks.
 			</p>
-			<p>
-				Hör gärna av er om det är något som är otydligt eller om det är något ni funderar på. Med vänliga
-				hälsningar, Ludvig Björn
-			</p>
+			<p>Hör gärna av er om det är något som är otydligt eller om det är något ni funderar på.</p>
+			<p style={{ fontWeight: 'bold' }}>Med vänliga hälsningar, Ludvig Björn</p>
 		</div>
 	);
 };
