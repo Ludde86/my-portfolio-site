e64470e5fc8563e482aca5dfba3089601cc996e5
Ludde86 <bslbjorn@gmail.com>
2020-04-01 10:32:55 +0200

    fix skillprogress

--- src/components/resume.js
+++ src/components/resume.js
@@ -75,7 +75,7 @@ const Resume = () => {
 					/>
 					<hr style={{ borderTop: '3px solid #e22947' }} />
 					<Skills skill="Kundservice   " progress={93} />
-					<Skills skill="Ordning & reda" progress={91} />
+					<Skills skill="Ordning & reda" progress={93} />
 					<Skills skill="Tempo         " progress={97} />
 					<Skills skill="Arbetsmoral   " progress={100} />
 				</Cell>
