dcd0ebf34fb367c665001a2929a15cc4f439c453
Ludde86 <bslbjorn@gmail.com>
2021-09-08 00:05:45 +0200

    add about me in landing

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -37,11 +37,32 @@ const Landing = () => {
 							<text className="info-text">Ludvig Björn</text>
 						</div>
 						<div className="skills-container">
-							<p className="skills-text">
-								React | React Native | JavaScript | HTML | CSS | Java | Spring
-							</p>
+							<p className="skills-text">React | React Native | JavaScript | HTML | CSS | Java</p>
 						</div>
 					</div>
+				</div>
+
+				<div className="about-me">
+					<p className="about-text">
+						Min karriär som utvecklare startade 2017 då jag genomförde två distanskurser, Programmering
+						Grund- och Fortsättningskurs i Java. Året därpå antog jag en tvåårig yh-utbildning som
+						Javautvecklare på IT-Högskolan i Liljeholmen.
+					</p>
+					<p className="about-text">
+						Vid sidan utav studierna har jag ägnat mig åt JavaScript och React/React Native, där jag skapat
+						ett flertal hemsidor och ett par appar.
+					</p>
+					<p className="about-text">
+						Mitt senaste projekt är en app utvecklat med React Native, en parkeringstjänst (likt easypark)
+						som tidigare enbart var webbaserad. Tjänsten, som har aktiva kunder, är inriktad till
+						företag/föreningar.
+					</p>
+
+					<p className="greeting-text">
+						Tveka inte på att höra av er om ni har frågor eller funderingar.
+						<br />
+						Med vänliga hälsningar, Ludvig Björn
+					</p>
 				</div>
 			</div>
 		</div>
