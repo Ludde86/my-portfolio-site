8e21dec65aecfb3bba9ac428e69857874882c1cb
Ludde86 <bslbjorn@gmail.com>
2024-03-22 14:49:15 +0100

    add servicespage

--- src/components/webdesigns/naprapatrehab2/services/ServicesNapra2.js
+++ src/components/webdesigns/naprapatrehab2/services/ServicesNapra2.js
@@ -4,8 +4,60 @@ import './servicesNapra2.css'
 const ServicesNapra2 = () => {
     return (
         <div className='services_napra2-container'>
-            <div>
-                ServicesNapra2
+
+            <div id='services_napra2-container_first-page1' className='services_napra2-container_first-page'>
+                <div className='services_napra2-container_first-page_text-container'>
+                    <h2 className='services_napra2-container_first-page_text-container_title'>
+                        Naprapatbehandling nybesök/återbesök 30-40 min
+                    </h2>
+
+                    <p className='services_napra2-container_first-page_text-container_text'>
+                        Naprapati är en manuell terapiform där du vid ett besök får diagnostisering, behandling och rehabilitering av smärttillstånd och funktionella besvär i skelett, muskler och leder. Naprapatbehandlingen syftar till att återställa kroppens normala rörlighet och funktion samt minska smärtan.
+                    </p>
+                    <p className='services_napra2-container_first-page_text-container_text'>
+                        För att uppnå detta tillämpas olika behandlingsmetoder och tekniker som ex. ledmanipulation och mobilisering, mjukdelsbehandling som massage, stretching, men även TENS och dry needling.
+                    </p>
+
+                    {/*
+                        <a id="booking-button" className='services_napra2-container_first-page_text-container_text' href='https://www.bokadirekt.se/places/naprapat-rehabcenter-haninge-10747' target='_blank' rel="noreferrer">
+                        Boka Direkt
+                        </a>
+                        */}
+
+                </div>
+            </div>
+
+            <div id='services_napra2-container_first-page2' className='services_napra2-container_first-page'>
+                <div className='services_napra2-container_first-page_text-container'>
+                    <h2 className='services_napra2-container_first-page_text-container_title'>
+                        Naprapatbehandling pensionär/student 30-40 min
+                    </h2>
+
+                    <p className='services_napra2-container_first-page_text-container_text'>
+                        Naprapati är en manuell terapiform där du vid ett besök får diagnostisering, behandling och rehabilitering av smärttillstånd och funktionella besvär i skelett, muskler och leder. Naprapatbehandlingen syftar till att återställa kroppens normala rörlighet och funktion samt minska smärtan.
+                    </p>
+                    <p className='services_napra2-container_first-page_text-container_text'>
+                        För att uppnå detta tillämpas olika behandlingsmetoder och tekniker som ex. ledmanipulation och mobilisering, mjukdelsbehandling som massage, stretching, men även TENS och dry needling.
+                    </p>
+
+                </div>
+            </div>
+
+            <div id='services_napra2-container_first-page3' className='services_napra2-container_first-page'>
+                <div className='services_napra2-container_first-page_text-container'>
+                    <h2 className='services_napra2-container_first-page_text-container_title'>
+                        Massage 30 min
+                    </h2>
+
+                    <p className='services_napra2-container_first-page_text-container_text'>
+                        Denna tjänst är till för dig som inte söker för några specifika besvär utan önskar enklare mjukdelsbehandling i form av massage, triggerpunktsbehandling och stretch.
+
+                        Vi lägger lite extra fokus på de områden i kroppen där du har mer muskulära spänningar.
+                    </p>
+
+
+
+                </div>
             </div>
 
         </div>
