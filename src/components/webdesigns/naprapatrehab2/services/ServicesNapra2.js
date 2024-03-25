259710feab84cfc7e70d59d0959a9177681d57b7
Ludde86 <bslbjorn@gmail.com>
2024-03-25 21:41:30 +0100

    fix servicepage css

--- src/components/webdesigns/naprapatrehab2/services/ServicesNapra2.js
+++ src/components/webdesigns/naprapatrehab2/services/ServicesNapra2.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import './servicesNapra2.css'
-import service1 from '../assets/img/DSC06469.jpeg'
-import service2 from '../assets/img/DSC06565.jpeg'
-import service3 from '../assets/img/DSC06362.jpeg'
+import brandLogo1 from '../assets/img/Epassi Logo.png'
+import brandLogo2 from '../assets/img/wellnet-logga.png'
 
 const ServicesNapra2 = () => {
     return (
@@ -13,9 +12,16 @@ const ServicesNapra2 = () => {
 
                 </div>
                 <div className='services_napra2-container_first-page_text-container'>
-                    <h2 className='services_napra2-container_first-page_text-container_title'>
-                        Naprapatbehandling nybesök/återbesök
-                    </h2>
+                    <div className='services_napra2-container_first-page_text-container_title-container'>
+                        <h2 className='services_napra2-container_first-page_text-container_title'>
+                            Naprapatbehandling Nybesök/återbesök
+                        </h2>
+                        <div className='services_napra2-container_first-page_text-container_price-container'>
+                            <p className='services_napra2-container_first-page_text-container_price'>
+                                750:-
+                            </p>
+                        </div>
+                    </div>
                     <h2 className='services_napra2-container_first-page_text-container_subtitle'>
                         30-40 min
                     </h2>
@@ -40,20 +46,25 @@ const ServicesNapra2 = () => {
                 <div id='services_napra2-container_first-page2' className='services_napra2-container_first-page_img-container'>
                 </div>
                 <div className='services_napra2-container_first-page_text-container'>
-                    <h2 className='services_napra2-container_first-page_text-container_title'>
-                        Naprapatbehandling pensionär/student
-                    </h2>
+                    <div className='services_napra2-container_first-page_text-container_title-container'>
+                        <h2 className='services_napra2-container_first-page_text-container_title'>
+                            Naprapatbehandling Pensionär/student
+                        </h2>
+                        <div className='services_napra2-container_first-page_text-container_price-container'>
+                            <p className='services_napra2-container_first-page_text-container_price'>
+                                700:-
+                            </p>
+                        </div>
+                    </div>
                     <h2 className='services_napra2-container_first-page_text-container_subtitle'>
                         30-40 min
                     </h2>
-
                     <p className='services_napra2-container_first-page_text-container_text'>
                         Naprapati är en manuell terapiform där du vid ett besök får diagnostisering, behandling och rehabilitering av smärttillstånd och funktionella besvär i skelett, muskler och leder. Naprapatbehandlingen syftar till att återställa kroppens normala rörlighet och funktion samt minska smärtan.
                     </p>
                     <p className='services_napra2-container_first-page_text-container_text'>
                         För att uppnå detta tillämpas olika behandlingsmetoder och tekniker som ex. ledmanipulation och mobilisering, mjukdelsbehandling som massage, stretching, men även TENS och dry needling.
                     </p>
-
                 </div>
             </div>
 
@@ -61,19 +72,82 @@ const ServicesNapra2 = () => {
                 <div id='services_napra2-container_first-page3' className='services_napra2-container_first-page_img-container'>
                 </div>
                 <div className='services_napra2-container_first-page_text-container'>
+                    <div className='services_napra2-container_first-page_text-container_title-container'>
+                        <h2 className='services_napra2-container_first-page_text-container_title'>
+                            Massage
+                        </h2>
+                        <div className='services_napra2-container_first-page_text-container_price-container'>
+                            <p className='services_napra2-container_first-page_text-container_price'>
+                                730:-
+                            </p>
+                        </div>
+                    </div>
                     <h2 className='services_napra2-container_first-page_text-container_subtitle'>
-                        Massage 30 min
+                        30 min
                     </h2>
-
                     <p className='services_napra2-container_first-page_text-container_text'>
                         Denna tjänst är till för dig som inte söker för några specifika besvär utan önskar enklare mjukdelsbehandling i form av massage, triggerpunktsbehandling och stretch.
 
                         Vi lägger lite extra fokus på de områden i kroppen där du har mer muskulära spänningar.
                     </p>
-
-
-
                 </div>
+            </div>
+
+            <div className='services_napra2-container_first-page'>
+                <div id='services_napra2-container_first-page4' className='services_napra2-container_first-page_img-container'>
+                </div>
+                <div className='services_napra2-container_first-page_text-container'>
+                    <div className='services_napra2-container_first-page_text-container_title-container'>
+                        <h2 className='services_napra2-container_first-page_text-container_title'>
+                            MammaMageträning Nybesök
+                        </h2>
+                        <div className='services_napra2-container_first-page_text-container_price-container'>
+                            <p className='services_napra2-container_first-page_text-container_price'>
+                                600:-
+                            </p>
+                        </div>
+                    </div>
+                    <h2 className='services_napra2-container_first-page_text-container_subtitle'>
+                        50 min
+                    </h2>
+                    <p className='services_napra2-container_first-page_text-container_text'>
+                        Som certifierad MammaMagetränare kan jag ge dig konsultationer kring magen, diasatser och dynfunktioner för att sedan tillsammans med dig hitta ett mer optimalt muskelengagemang.
+                    </p>
+                    <p className='services_napra2-container_first-page_text-container_text'>
+                        Första besöket innebär ofta mycket prat, inhämtning av information (anamnes), inspektion och muskelfunktionstest av aktuell muskulatur ofta djup bäckenmuskulatur och bålkontroll. Då kommer vi att testa muskulaturen i olika positioner för att hitta en eller flera positioner där du känner att du har kontroll och där jag kan se att du kan rekrytera muskulaturen på ett bra sätt. Därefter kommer du att få 1-3 hemövningar som du får med dig hem att göra till nästa gång vi ses.
+                    </p>
+                </div>
+            </div>
+
+            <div className='services_napra2-container_first-page' style={{ marginBottom: '64px' }}> {/* last service container */}
+                <div id='services_napra2-container_first-page4' className='services_napra2-container_first-page_img-container'>
+                </div>
+                <div className='services_napra2-container_first-page_text-container'>
+                    <div className='services_napra2-container_first-page_text-container_title-container'>
+                        <h2 className='services_napra2-container_first-page_text-container_title'>
+                            MammaMageträning Återbesök
+                        </h2>
+                        <div className='services_napra2-container_first-page_text-container_price-container'>
+                            <p className='services_napra2-container_first-page_text-container_price'>
+                                550:-
+                            </p>
+                        </div>
+                    </div>
+                    <h2 className='services_napra2-container_first-page_text-container_subtitle'>
+                        40 min
+                    </h2>
+                    <p className='services_napra2-container_first-page_text-container_text'>
+                        Som certifierad MammaMagetränare kan jag ge dig konsultationer kring magen, diasatser och dynfunktioner för att sedan tillsammans med dig hitta ett mer optimalt muskelengagemang.
+                    </p>
+                    <p className='services_napra2-container_first-page_text-container_text'>
+                        Första besöket innebär ofta mycket prat, inhämtning av information (anamnes), inspektion och muskelfunktionstest av aktuell muskulatur ofta djup bäckenmuskulatur och bålkontroll. Då kommer vi att testa muskulaturen i olika positioner för att hitta en eller flera positioner där du känner att du har kontroll och där jag kan se att du kan rekrytera muskulaturen på ett bra sätt. Därefter kommer du att få 1-3 hemövningar som du får med dig hem att göra till nästa gång vi ses.
+                    </p>
+                </div>
+            </div>
+
+            <div className='services_napra2-container_first-page_brands-container'>
+                <img src={brandLogo1} alt="brandLogo1" />
+                <img src={brandLogo2} alt="brandLogo2" />
             </div>
 
         </div>
