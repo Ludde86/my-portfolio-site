7c46284bd41fed20d869065190335f399effa3db
Ludde86 <bslbjorn@gmail.com>
2024-04-08 18:15:53 +0200

    add sidebar with animated button

--- src/components/webdesigns/haningetk1/Haningetk1.js
+++ src/components/webdesigns/haningetk1/Haningetk1.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './haningetk1.css'
 import NavbarHtk1 from './navbar/NavbarHtk1'
+import SidebarHtk1 from './sidebar/SidebarHtk1'
 
 const Haningetk1 = () => {
+
+    const [openSidebar, setOpenSidebar] = useState(false);
+
+    const onSetOpenSidebar = () => {
+        setOpenSidebar(!openSidebar)
+    }
+
     return (
         <div className='haningetk1-container'>
-            <NavbarHtk1 />
+            <NavbarHtk1 onSetOpenSidebar={onSetOpenSidebar} />
+            <SidebarHtk1 openSidebar={openSidebar} onSetOpenSidebar={onSetOpenSidebar} />
         </div>
     )
 }
--- src/components/webdesigns/haningetk1/sidebar/SidebarHtk1.js
+++ src/components/webdesigns/haningetk1/sidebar/SidebarHtk1.js
@@ -1,59 +1,53 @@
 import React from 'react'
-import './navbarhtk1.css'
+import './sidebarhtk1.css'
 
-const NavbarHtk1 = ({ onSetOpenSidebar }) => {
-
+const SidebarHtk1 = ({ openSidebar, onSetOpenSidebar }) => {
     return (
-        <div className='htk-navbar-container'>
+        <div className='htk1-sidebar-container' style={{
+            width: openSidebar && '50%'
+        }} onClick={() => onSetOpenSidebar()} >
 
 
 
             <div className='htk-navbar-container_links-container_link-container'>
-                <a className='htk-navbar-container_links-container_icon' to="/projects/home">
-                    <i className="fa fa-home" aria-hidden="true" style={{ color: '#fff' }} />
-                </a>
-                <div className='htk-navbar-container_links-container_link_underline'></div>
+                <span id='htk1-sidebar-button' className='htk-navbar-container_links-container_icon' onClick={() => onSetOpenSidebar()}>
+                    <i className={openSidebar ? "fa fa-times" : "fa fa-align-left"} aria-hidden="true" style={{ color: '#fff' }} />
+                </span>
             </div>
-
-            <div className='htk-navbar-container_links-container'>
+            <div className='htk-sidebar-container_links-container' style={{
+                marginLeft: openSidebar ? 0 : '-100vw'
+            }}>
                 <div className='htk-navbar-container_links-container_link-container'>
                     <a className='htk-navbar-container_links-container_link' href="/projects/">
-                        Om klubben
+                        Tennisskolan
                     </a>
                     <div className='htk-navbar-container_links-container_link_underline'></div>
                 </div>
 
                 <div className='htk-navbar-container_links-container_link-container'>
                     <a className='htk-navbar-container_links-container_link' href="/projects/">
-                        Kontakt
+                        Tävling
                     </a>
                     <div className='htk-navbar-container_links-container_link_underline'></div>
                 </div>
 
                 <div className='htk-navbar-container_links-container_link-container'>
                     <a className='htk-navbar-container_links-container_link' href="/projects/">
-                        Medlemsskap
+                        Gruppspel
                     </a>
                     <div className='htk-navbar-container_links-container_link_underline'></div>
                 </div>
 
                 <div className='htk-navbar-container_links-container_link-container'>
                     <a className='htk-navbar-container_links-container_link' href="/projects/">
-                        Vision & värdegrund
+                        Kontrakt- och strötider
                     </a>
                     <div className='htk-navbar-container_links-container_link_underline'></div>
                 </div>
 
                 <div className='htk-navbar-container_links-container_link-container'>
-                    <a className='htk-navbar-container_links-container_link' href="/projects/">
-                        Hitta hit
-                    </a>
-                    <div className='htk-navbar-container_links-container_link_underline'></div>
-                </div>
-
-                <div className='htk-navbar-container_links-container_link-container'>
-                    <a className='htk-navbar-container_links-container_link' href='https://www.stadium.se/foreningar/2436683' target='_blank' rel="noreferrer">
-                        Shopen
+                    <a className='htk-navbar-container_links-container_link' href='https://www.matchi.se/facilities/haningetk' target='_blank' rel="noreferrer">
+                        Boka bana
                     </a>
                     <div className='htk-navbar-container_links-container_link_underline'></div>
                 </div>
@@ -62,4 +56,4 @@ const NavbarHtk1 = ({ onSetOpenSidebar }) => {
     )
 }
 
-export default NavbarHtk1
+export default SidebarHtk1
