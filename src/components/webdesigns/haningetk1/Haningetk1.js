a26137978fc1318dbf1fa4bda4444347f1540b3a
Ludde86 <bslbjorn@gmail.com>
2024-04-09 16:50:12 +0200

    add fotterpage and footer media

--- src/components/webdesigns/haningetk1/Haningetk1.js
+++ src/components/webdesigns/haningetk1/Haningetk1.js
@@ -3,6 +3,7 @@ import './haningetk1.css'
 import Htk1AboutPage from './about/Htk1AboutPage'
 import Htk1LandingPage from './landing/Htk1LandingPage'
 import Htk1VisionsPage from './visions/Htk1VisionsPage'
+import Htk1FooterPage from './footer/Htk1FooterPage'
 
 const Haningetk1 = () => {
 
@@ -17,6 +18,7 @@ const Haningetk1 = () => {
             <Htk1LandingPage onSetOpenSidebar={onSetOpenSidebar} openSidebar={openSidebar} />
             <Htk1AboutPage />
             <Htk1VisionsPage />
+            <Htk1FooterPage />
             {/*
             <img className='htl1-logo' src={logo} alt='logo' />
         */}
