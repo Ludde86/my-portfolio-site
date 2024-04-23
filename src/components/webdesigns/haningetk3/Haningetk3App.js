bfb6b8f96b603696a34c8ffc15d4171376df1fb7
Ludde86 <bslbjorn@gmail.com>
2024-04-23 10:18:41 +0200

    add footer

--- src/components/webdesigns/haningetk3/Haningetk3App.js
+++ src/components/webdesigns/haningetk3/Haningetk3App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./haningetk3App.css";
 import Haningetk3Welcome from "./welcome/Haningetk3Welcome";
 import Haningetk3Landing from "./landing/Haningetk3Landing";
+import Haningetk3Footer from "./footer/Haningetk3Footer";
 
 const Haningetk3App = () => {
   return (
@@ -10,6 +11,7 @@ const Haningetk3App = () => {
         <Haningetk3Welcome />
         <Haningetk3Landing />
       </div>
+      <Haningetk3Footer />
     </div>
   );
 };
---
+++ src/components/webdesigns/haningetk3/footer/Haningetk3Footer.js
@@ -0,0 +1,41 @@
+import React from "react";
+import "./haninge3Footer.css";
+
+import { FaInstagram, FaFacebookSquare } from "react-icons/fa";
+
+const Haningetk3Footer = () => {
+  return (
+    <div className="htk3-footer_container">
+      <div className="htk3-footer_container-contact_container-open">
+        <p>06.30-23.30 </p>
+      </div>
+      <div className="htk3-footer_container-contact_container">
+        <div className="htk3-footer_container-contact_container-contact">
+          <h4>Adress</h4>
+          <p>Torvallavägen 14 13644 Handen</p>
+        </div>
+
+        <div className="htk3-footer_container-contact_container-contact">
+          <h4>Epost</h4>
+          <p>kansliet@haningetk.se</p>
+        </div>
+
+        <div className="htk3-footer_container-contact_container-contact">
+          <h4>Telefon</h4>
+          <p>0700000000</p>
+        </div>
+      </div>
+
+      <div className="htk3-footer_container-social">
+        <a>
+          <FaInstagram />
+        </a>
+        <a>
+          <FaFacebookSquare />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Haningetk3Footer;
