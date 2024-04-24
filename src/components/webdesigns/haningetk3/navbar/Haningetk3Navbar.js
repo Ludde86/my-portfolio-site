64c109b9a1e52bc8133357ee1bf85209f5fafc83
Ludde86 <bslbjorn@gmail.com>
2024-04-24 11:40:01 +0200

    add sub navbar

--- src/components/webdesigns/haningetk3/navbar/Haningetk3Navbar.js
+++ src/components/webdesigns/haningetk3/navbar/Haningetk3Navbar.js
@@ -1,19 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import "./haningetk3Navbar.css";
 import { FiAlignJustify } from "react-icons/fi";
 
 const Haningetk3Navbar = () => {
+  const [expandNavbar, setExpandNavbar] = useState(false);
+
   return (
-    <div className="htk3-navbar_container">
-      <a href="/">Tennisskola</a>
-      <a href="/">Medlemsskap</a>
-      <a id="htk3-navbar_container-icon" href="/">
-        <FiAlignJustify />
-      </a>
-      <a href="/">Om oss</a>
-      <a id="htk3-navbar_container-blue" href="/">
-        Boka bana
-      </a>
+    <div
+      className="htk3-navbar_container"
+      style={{
+        minHeight: expandNavbar && "80vh",
+        background: expandNavbar && "#000000a8",
+      }}
+    >
+      <div className="htk3-navbar_container-main">
+        <a href="/">Tennisskola</a>
+        <a href="/">Medlemsskap</a>
+        <div
+          id="htk3-navbar_container-icon"
+          onClick={() => setExpandNavbar(!expandNavbar)}
+        >
+          <FiAlignJustify />
+        </div>
+        <a href="/">Om oss</a>
+        <a id="htk3-navbar_container-blue" href="/">
+          Boka bana
+        </a>
+      </div>
+
+      <div
+        className="htk3-navbar_container-sub"
+        style={{ visibility: expandNavbar && "visible" }}
+      >
+        <a href="/">Vision och värdegrunde</a>
+        <a href="/">Träna och spela hos oss</a>
+        <a href="/">Kontrakt- och strötider</a>
+        <a href="/">Tävlingar</a>
+        <a href="/">Gruppspel</a>
+        <a href="/">Styrelse</a>
+        <a href="/">Klubbens historia</a>
+        <a href="/">Kontakt</a>
+      </div>
     </div>
   );
 };
