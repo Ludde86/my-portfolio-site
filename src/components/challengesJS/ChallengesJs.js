663acc9a2c93a25136c04dd937defb6ff91829a0
Ludde86 <bslbjorn@gmail.com>
2024-05-14 13:52:09 +0200

    add next & prev func

--- src/components/challengesJS/ChallengesJs.js
+++ src/components/challengesJS/ChallengesJs.js
@@ -3,6 +3,7 @@ import "./challengesjs.css";
 import Js1App from "./js1/Js1App";
 import Js2App from "./js2imageslider1/Js2App";
 import Js3App from "./js3imageslider2/Js3App";
+import Js4App from "./js4imageslider3/Js4App";
 
 const ChallengesJs = () => {
   return (
@@ -10,6 +11,7 @@ const ChallengesJs = () => {
       <Js1App />
       <Js2App />
       <Js3App />
+      <Js4App />
     </div>
   );
 };
---
+++ src/components/challengesJS/js4imageslider3/Js4App.js
@@ -0,0 +1,93 @@
+import React, { useEffect, useState } from "react";
+import "./js4App.css";
+import img1 from "../../../img/bridge.jpg";
+import img2 from "../../../img/green-gekko.jpg";
+import img3 from "../../../img/nature-beginning.jpg";
+import img4 from "../../../img/round-drop.jpg";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+
+const imagesData = [
+  { url: img1, title: "Bild1" },
+  { url: img2, title: "Bild2" },
+  { url: img3, title: "Bild3" },
+  { url: img4, title: "Bild4" },
+];
+
+const Js4App = () => {
+  const [image, setImage] = useState(0);
+  const [animationNext, setAnimationNext] = useState(false);
+  const [animationPrev, setAnimationPrev] = useState(false);
+
+  useEffect(() => {
+    setAnimationNext(true);
+
+    setTimeout(() => {
+      setAnimationNext(false);
+    }, "1000");
+  }, []);
+
+  const handleSetImage = (direction) => {
+    if (direction === "next") {
+      if (image === imagesData.length - 1) {
+        setImage(0);
+      } else {
+        setImage(image + 1);
+      }
+      setAnimationNext(true);
+
+      setTimeout(() => {
+        setAnimationNext(false);
+      }, "1000");
+    } else {
+      if (image === 0) {
+        setImage(imagesData.length - 1);
+      } else {
+        setImage(image - 1);
+      }
+      setAnimationPrev(true);
+
+      setTimeout(() => {
+        setAnimationPrev(false);
+      }, "1000");
+    }
+  };
+
+  return (
+    <div className="js4_container">
+      {animationNext ? (
+        <img
+          src={imagesData[image].url}
+          alt="img1"
+          className="js4_container-image js4_container-animation_next"
+        />
+      ) : (
+        <img
+          hidden={animationPrev}
+          src={imagesData[image].url}
+          alt="img1"
+          className="js4_container-image"
+        />
+      )}
+
+      {animationPrev && (
+        <img
+          src={imagesData[image].url}
+          alt="img1"
+          className="js4_container-image js4_container-animation_prev"
+        />
+      )}
+
+      <div className="js4_container-button_container">
+        <button onClick={() => handleSetImage("prev")}>
+          <ChevronLeft fill="#fff" stroke="#fff" />
+        </button>
+        <p>{imagesData[image].title}</p>
+        <button onClick={() => handleSetImage("next")}>
+          <ChevronRight fill="#fff" stroke="#fff" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Js4App;
