30af86d5c735b218658772abd802588064797af7
Ludde86 <bslbjorn@gmail.com>
2024-05-29 13:11:39 +0200

    add navbar

--- src/components/webdesigns/lilitha1/landing/Lil1Landing.js
+++ src/components/webdesigns/lilitha1/landing/Lil1Landing.js
@@ -1,14 +1,18 @@
 import React from "react";
 import "./lil1Landing.css";
+import Lil1Navbar from "../navbar/Lil1Navbar";
 
 const Lil1Landing = () => {
   return (
     <div className="lil1_landing_container grid-container">
+      <Lil1Navbar />
       <div className="grid-item">1</div>
       <div className="grid-item">2</div>
       <div className="lil1_landing_container-welcome_text grid-item">
         <h1>Välkommen till</h1>
-        <h1>Lilitha Hex Design</h1>
+        <h1 className="color_gold" style={{ fontSize: "3rem" }}>
+          Lilitha Hex Design
+        </h1>
       </div>
       <div className="grid-item">4</div>
     </div>
---
+++ src/components/webdesigns/lilitha1/navbar/Lil1Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./lil1Navbar.css";
+
+const Lil1Navbar = () => {
+  return (
+    <div className="lil1_navbar_container">
+      <a href="/">Hem</a>
+      <a href="/">Om Mig</a>
+      <a href="/">Kontakt</a>
+      <a href="/">Produkter</a>
+    </div>
+  );
+};
+
+export default Lil1Navbar;
