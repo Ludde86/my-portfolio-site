ae1d96c9bf11c3e999c0b94142bbf35948760653
Ludde86 <bslbjorn@gmail.com>
2024-05-29 13:52:00 +0200

    add content1 page

--- src/components/webdesigns/lilitha1/Lilitha1.js
+++ src/components/webdesigns/lilitha1/Lilitha1.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./lilitha.css";
 import Lil1Landing from "./landing/Lil1Landing";
+import Lil1Content1 from "./content1/Lil1Content1";
+import Lil1About from "./about/Lil1About";
 
 const Lilitha1 = () => {
   return (
     <div className="lilitha1_container">
       <Lil1Landing />
+      <Lil1About />
+      <Lil1Content1 />
     </div>
   );
 };
---
+++ src/components/webdesigns/lilitha1/about/Lil1About.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./lil1About.css";
+
+const Lil1About = () => {
+  return (
+    <div className="lil1_about_container">
+      <div className="lil1_about_container-title_container">
+        <h3 className="color_gold">Om Mig:</h3>
+      </div>
+      <div className="lil1_about_container-desc_container">
+        <p>
+          Jag älskar att göra mina armband och lägger alltid in beskydd,
+          självförtroende och kärlek i dem. Det blir liksom som en liten
+          trollramsa som jag har. Detta är min själs terapi och gör mig gott.
+          Att göra armband gör mig lycklig helt enkelt.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Lil1About;
