b60e21cda5ca2adb93f75f11cf58221a91ad318c
Ludde86 <bslbjorn@gmail.com>
2024-05-29 12:44:16 +0200

    add landing page

---
+++ src/components/webdesigns/lilitha1/Lilitha1.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./lilitha.css";
+import Lil1Landing from "./landing/Lil1Landing";
+
+const Lilitha1 = () => {
+  return (
+    <div className="lilitha1_container">
+      <Lil1Landing />
+    </div>
+  );
+};
+
+export default Lilitha1;
---
+++ src/components/webdesigns/lilitha1/landing/Lil1Landing.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./lil1Landing.css";
+
+const Lil1Landing = () => {
+  return (
+    <div className="lil1_landing_container grid-container">
+      <div className="grid-item">1</div>
+      <div className="grid-item">2</div>
+      <div className="lil1_landing_container-welcome_text grid-item">
+        <h1>Välkommen till</h1>
+        <h1>Lilitha Hex Design</h1>
+      </div>
+      <div className="grid-item">4</div>
+    </div>
+  );
+};
+
+export default Lil1Landing;
