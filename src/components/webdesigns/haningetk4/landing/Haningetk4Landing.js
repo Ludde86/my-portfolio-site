7e72250ac2b8ef72f05389be83368df7bc834026
Ludde86 <bslbjorn@gmail.com>
2024-06-05 11:58:59 +0200

    add media @ nav and landing

--- src/components/webdesigns/haningetk4/landing/Haningetk4Landing.js
+++ src/components/webdesigns/haningetk4/landing/Haningetk4Landing.js
@@ -4,20 +4,29 @@ import "./haningetk4Landing.css";
 const Haningetk4Landing = () => {
   return (
     <div className="haninge4_landing">
-      <div className="htk4_landing-grid_content haninge4_landing-welcome_text">
-        <h3>Välkommen till</h3>
-        <h1>Haninge Tennisklubb</h1>
+      <div className="htk4_landing-grid_content">
+        <div className="haninge4_landing-welcome_text">
+          <h3>Välkommen till</h3>
+          <h1>Haninge Tennisklubb</h1>
+        </div>
       </div>
       <div className="htk4_landing-grid_content"></div>
       <div className="htk4_landing-grid_content">
         <button className="haninge4_landing-button haninge4_landing-action_button">
-          Boka bana
+          Bli medlem
         </button>
       </div>
       <div className="htk4_landing-grid_content">
         <button className="haninge4_landing-button haninge4_landing-sub_button">
-          Bli medlem
+          Boka bana
         </button>
+      </div>
+      <div className="haninge4_landing-vision_text">
+        <p>
+          Vi är den personliga tennisklubben där alla ska känna sig lika
+          välkomna, där individen ges möjlighet till utveckling och efter sin
+          egen ambitionsnivå uppnå sin högsta potential.
+        </p>
       </div>
     </div>
   );
