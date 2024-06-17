500361ae8b9426916e59e5bf62ee504cb5c0594b
Ludde86 <bslbjorn@gmail.com>
2024-06-17 15:14:44 +0200

    add colors @results

--- src/components/challengesJS/js8RockPaperScissorsGame/Js8App.js
+++ src/components/challengesJS/js8RockPaperScissorsGame/Js8App.js
@@ -18,33 +18,132 @@ In this project, we create a rock, paper, and scissors game. The computer will a
 const Js8App = () => {
   const [userPoints, setUserPoints] = useState(0);
   const [comPoints, setComPoints] = useState(0);
+  const [userSelected, setUserSelected] = useState("");
+  const [comSelected, setComSelected] = useState("");
+  const [textRestult, setTextRestult] = useState("");
+
+  const gameChoices = ["ROCK", "SCISSORS", "PAPER"];
+
+  const handleUserSelected = (selected, com) => {
+    console.log("clicked");
+    console.log("selected: ", selected);
+    console.log("com: ", com);
+
+    setUserSelected(selected);
+    setComSelected(com);
+
+    if (selected === "ROCK") {
+      if (com === "ROCK") {
+        setTextRestult("LIKA!");
+      }
+      if (com === "SCISSORS") {
+        setUserPoints((userPoints) => userPoints + 1);
+        setTextRestult("Du vann! Sten slår sax.");
+      }
+      if (com === "PAPER") {
+        setTextRestult("Du förlorade. Påse slår sten.");
+        setComPoints((comPoints) => comPoints + 1);
+      }
+    }
+
+    if (selected === "SCISSORS") {
+      if (com === "SCISSORS") {
+        setTextRestult("LIKA!");
+      }
+      if (com === "PAPER") {
+        setTextRestult("Du vann! Sax slår påse.");
+        setUserPoints((userPoints) => userPoints + 1);
+      }
+      if (com === "ROCK") {
+        setTextRestult("Du förlorade. Sten slår sax.");
+        setComPoints((comPoints) => comPoints + 1);
+      }
+    }
+
+    if (selected === "PAPER") {
+      if (com === "PAPER") {
+        setTextRestult("LIKA!");
+      }
+      if (com === "ROCK") {
+        setTextRestult("Du vann! Påse slår sten.");
+        setUserPoints((userPoints) => userPoints + 1);
+      }
+      if (com === "SCISSORS") {
+        setTextRestult("Du förlorade. Sax slår påse.");
+        setComPoints((comPoints) => comPoints + 1);
+      }
+    }
+  };
 
   return (
     <div className="js8_container">
+      <h1>#8 - Rock Paper Scissors</h1>
       <div className="js8_container-game_container">
         <h3>Välj ditt drag:</h3>
         <div className="js8_container-game_container-icons">
           <div
             className="js8_container-game_container-icons-icon"
             id="js8_container-game_container-icons-icon-stone"
+            onClick={() =>
+              handleUserSelected(
+                "ROCK",
+                gameChoices[Math.floor(Math.random() * gameChoices.length)]
+              )
+            }
           >
             <LiaHandRockSolid />
           </div>
           <div
             className="js8_container-game_container-icons-icon"
             id="js8_container-game_container-icons-icon-scissors"
+            onClick={() =>
+              handleUserSelected(
+                "SCISSORS",
+                gameChoices[Math.floor(Math.random() * gameChoices.length)]
+              )
+            }
           >
             <LiaHandScissorsSolid />
           </div>
           <div
             className="js8_container-game_container-icons-icon"
             id="js8_container-game_container-icons-icon-paper"
+            onClick={() =>
+              handleUserSelected(
+                "PAPER",
+                gameChoices[Math.floor(Math.random() * gameChoices.length)]
+              )
+            }
           >
             <LiaHandPaperSolid />
           </div>
         </div>
-        <h3>Dina poäng: {userPoints}</h3>
-        <h3>Datorns poäng: {comPoints}</h3>
+
+        {textRestult && (
+          <div className="js8_container-game_container-choice_text_container">
+            <div className="js8_container-game_container-choice_text_container-text">
+              <p>
+                *Du valde: {userSelected}, Datorn valde: {comSelected}
+              </p>
+            </div>
+          </div>
+        )}
+
+        <h3>{textRestult && textRestult}</h3>
+        <div className="js8_container-game_container-choice_text_container-text-points">
+          <h3>
+            Dina poäng:{" "}
+            <span className="js8_container-game_container-choice_text_container-text-blue">
+              {userPoints}
+            </span>
+          </h3>
+          <h3>
+            Datorns poäng:{" "}
+            <span className="js8_container-game_container-choice_text_container-text-red">
+              {comPoints}
+            </span>
+          </h3>
+        </div>
       </div>
     </div>
   );
