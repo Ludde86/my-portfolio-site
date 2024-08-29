3c8a9395d127fa511957158cc8a71f96e30b66ec
Ludde86 <bslbjorn@gmail.com>
2024-08-29 14:37:03 +0200

    add about page

--- src/components/webdesigns/haningetk3/desc2/Haningetk3Desc2.js
+++ src/components/webdesigns/haningetk3/desc2/Haningetk3Desc2.js
@@ -1,29 +1,34 @@
 import React from "react";
 import "./haningeDesc2.css";
 import bana1 from "../assets/img/utebana-1.jpg";
+import PageHeaderHtk3 from "../components/pageHeader/PageHeaderHtk3";
 
 const Haningetk3Desc2 = () => {
   return (
     <div className="htk3-desc2_container">
-      <div className="htk3-desc2_container-desc">
-        <p>
-          Haninge TK är en ideell tennisklubb belägen intill Torvalla
-          idrottsanläggning. Vi har två nymålade banor med Plexipave beläggning.
-          Dessutom har vi två grusbanor vid Sågen i Vendelsö.
-        </p>
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://www.matchi.se/facilities/haningetk"
-        >
-          <button className="hero_container-button-act_button">
-            Boka bana
-          </button>
-        </a>
+      <PageHeaderHtk3 header={"Våra banor"} />
+      <div className="htk3-desc2_container-content">
+        <div className="htk3-desc2_container-desc">
+          <p>
+            Haninge TK är en ideell tennisklubb belägen intill Torvalla
+            idrottsanläggning. Vi har två nymålade banor med Plexipave
+            beläggning. Dessutom har vi två grusbanor vid Sågen i Vendelsö.
+          </p>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.matchi.se/facilities/haningetk"
+          >
+            <button className="hero_container-button-act_button">
+              Boka bana
+            </button>
+          </a>
+        </div>
+        <div className="htk3-desc2_container-empty">
+          <img src={bana1} alt="bana1" />
+        </div>
       </div>
-      <div className="htk3-desc2_container-empty">
-        <img src={bana1} alt="bana1" />
-      </div>
+      <div></div>
     </div>
   );
 };
