e0894393ee0ae23638e3c778c6bd3b135404cc1c
Ludde86 <bslbjorn@gmail.com>
2024-09-03 22:16:39 +0200

    fix screen width media @desc2page

--- src/components/webdesigns/haningetk3/desc2/Haningetk3Desc2.js
+++ src/components/webdesigns/haningetk3/desc2/Haningetk3Desc2.js
@@ -12,28 +12,54 @@ const Haningetk3Desc2 = () => {
         header={"Våra banor"}
         color={screenWidth < 560 ? "#fff" : undefined}
       />
-      <div className="htk3-desc2_container-content">
-        <div className="htk3-desc2_container-empty">
-          <img src={bana1} alt="bana1" />
+      {screenWidth < 560 ? (
+        <div className="htk3-desc2_container-content">
+          <div className="htk3-desc2_container-desc">
+            <p>
+              Haninge TK är en ideell tennisklubb belägen intill Torvalla
+              idrottsanläggning. Vi har två nymålade banor med Plexipave
+              beläggning. Dessutom har vi två grusbanor vid Sågen i Vendelsö.
+            </p>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.matchi.se/facilities/haningetk"
+            >
+              <button className="hero_container-button-act_button">
+                Boka bana
+              </button>
+            </a>
+          </div>
+          <div className="htk3-desc2_container-empty">
+            <img src={bana1} alt="bana1" />
+          </div>
         </div>
-        <div className="htk3-desc2_container-desc">
-          <p>
-            Haninge TK är en ideell tennisklubb belägen intill Torvalla
-            idrottsanläggning. Vi har två nymålade banor med Plexipave
-            beläggning. Dessutom har vi två grusbanor vid Sågen i Vendelsö.
-          </p>
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://www.matchi.se/facilities/haningetk"
-          >
-            <button className="hero_container-button-act_button">
-              Boka bana
-            </button>
-          </a>
-        </div>
-      </div>
-      <div></div>
+      ) : (
+        <>
+          <div className="htk3-desc2_container-content">
+            <div className="htk3-desc2_container-empty">
+              <img src={bana1} alt="bana1" />
+            </div>
+            <div className="htk3-desc2_container-desc">
+              <p>
+                Haninge TK är en ideell tennisklubb belägen intill Torvalla
+                idrottsanläggning. Vi har två nymålade banor med Plexipave
+                beläggning. Dessutom har vi två grusbanor vid Sågen i Vendelsö.
+              </p>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.matchi.se/facilities/haningetk"
+              >
+                <button className="hero_container-button-act_button">
+                  Boka bana
+                </button>
+              </a>
+            </div>
+          </div>
+          <div></div>
+        </>
+      )}
     </div>
   );
 };
